56f8348d3553ee8f314cee238fedd7143f840e84
Irsyad Nurdin <irsyadnurdin12@gmail.com>
2026-04-23 08:32:51 +0700

    UPDATE

--- response/response.go
+++ response/response.go
@@ -1,11 +1,21 @@
 package response
 
+import "github.com/Gongaji-Apps/GONGAJI-COMMON/pagination"
+
 type Meta struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
 }
 
 type Response struct {
-	Meta Meta        `json:"meta"`
-	Data interface{} `json:"data,omitempty"`
+	Status_Code int              `json:"status_code"`
+	Status      bool             `json:"status"`
+	Message     string           `json:"message"`
+	Data        any              `json:"data,omitempty"`
+	Data_Total  *int64           `json:"data_total,omitempty"`
+	Pagination  *pagination.Meta `json:"pagination,omitempty"`
+	Token       *string          `json:"token,omitempty"`
+	Errors      any              `json:"errors,omitempty"`
+	Error_Code  *string          `json:"error_code,omitempty"`
+	Meta        Meta             `json:"meta"`
 }
