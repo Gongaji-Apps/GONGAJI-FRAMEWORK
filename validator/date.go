c2e25e70dd4230b1f6fec240571b765aa0871829
Irsyad Nurdin <irsyadnurdin12@gmail.com>
2026-05-08 14:57:42 +0700

    UPDATE

--- validator/date.go
+++ validator/date.go
@@ -19,8 +19,21 @@ func validateDate(fl validator.FieldLevel) bool {
 	return err == nil
 }
 
+func validateDateNullable(fl validator.FieldLevel) bool {
+	value := strings.TrimSpace(fl.Field().String())
+
+	if value == "" {
+		return true
+	}
+
+	_, err := time.Parse("2006-01-02", value)
+
+	return err == nil
+}
+
 func init() {
 	Register(func(v *validator.Validate) {
 		v.RegisterValidation("date", validateDate)
+		v.RegisterValidation("date_nullable", validateDateNullable)
 	})
 }
